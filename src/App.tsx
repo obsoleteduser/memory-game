73a4dd870849df06e341743f4c4b04bc268c3c68
Tahir <dibirovtahir@gmail.com>
2023-01-27 05:02:17 +0400

    INIT

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,72 @@
-import { FC, ReactElement, useEffect, useRef, useState } from 'react'
+import React, { FC, ReactElement, useEffect, useRef, useState } from 'react'
 import './App.css'
 
-interface ICompare{
+interface ICompare {
   color: string,
   active: boolean
 }
 
 
-function App(){
- 
- const [colors, setColors] = useState<string[]>([])
- const [colorActive, setColorActive] = useState<string[]>([])
- const [compare, setCompare] = useState<ICompare[]>([])
- console.log(colorActive)
+function App() {
 
- useEffect(()=>{
+  const [colors, setColors] = useState<string[]>([])
+  const [colorActive, setColorActive] = useState<string[]>([])
+  const [compare, setCompare] = useState<ICompare[]>([])
+  console.log(colorActive)
 
-  setColors([])
-  generateColors()
-  console.log(colors)
-  
+  useEffect(() => {
 
- }, [])
+    setColors([])
+    generateColors()
+    console.log(colors)
 
- function generateColors():void{
 
-  for(let i=0; i<4; i++){
-    setColors(prev => [...prev, Math.floor((Math.random()*16777215)).toString(16)])
+  }, [])
+
+  function generateColors(): void {
+
+    for (let i = 0; i < 4; i++) {
+      setColors(prev => [...prev, Math.floor((Math.random() * 16777215)).toString(16)])
+    }
+
+    setColors(prev => [...prev, ...prev])
+    setColors(prev => shuffle(prev))
+
   }
 
-  setColors(prev=>[...prev, ...prev])
-  setColors(prev => shuffle(prev))
 
- }
-
-
- function shuffle(array:Array<string>) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+  function shuffle(array: Array<string>) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
-  return array;
-}
 
 
 
 
-const getColor = async (event: any) =>{
+  const getColor = async (event: React.MouseEvent<HTMLElement>) => {
+
+    console.log(event.currentTarget.getAttribute('color'))
+    setCompare([...compare, {color: event?.currentTarget?.getAttribute('color'), active: true }])
+
+  }
+
+  if(compare.length===2 && compare[0].color === compare[1].color){
+    setColorActive([...colorActive, compare[0].color])
+    setCompare([])
+  }
   
-  event.stopPropagation()
-  console.log(event.currentTarget.getAttribute('color'))
-  setColorActive([...colorActive, event?.currentTarget?.getAttribute('color')!])
- 
-}
-
 
   return (
     <div className="App">
-      
+
       <div className="color-container">
 
         {
           colors.map((color, index) => (
-            <div onClick={getColor} className="color" key={index} color={`#${color}`} style={colorActive? {backgroundColor: `#${color}`}: {backgroundColor: 'black'}}></div>
+            <div onClick={getColor} className="color" key={index} color={`#${color}`} style={colorActive ? { backgroundColor: `#${color}` } : { backgroundColor: 'black' }}></div>
           ))
         }
 
